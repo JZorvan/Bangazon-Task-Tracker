d038339af0cbcd7f379bca50f97af4cb2429c090
JZorvan <jzorvan@gmail.com>
2017-01-16 18:08:21 -0800

    start context

--- BangazonTaskTracker/Models/Task.cs
+++ BangazonTaskTracker/Models/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,23 @@ namespace BangazonTaskTracker.Models
 {
     public class Task
     {
+        [Key]
+        [Required]
+        public int TaskId { get; set;}
+
+        [Required]
+        public string TaskName { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        [Required]
+        public Status TaskStatus { get; set; }
+
+        public DateTime CompletedOn { get; set; }
+    }
+
+    public enum Status
+    {
+        ToDo, InProgress, Complete
     }
 }
