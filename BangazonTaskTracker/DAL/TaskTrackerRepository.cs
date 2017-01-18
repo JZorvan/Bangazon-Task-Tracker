b683312a74a420bb570726560fb1df286c51e3b3
Janelle Zorvan <jzorvan@gmail.com>
2017-01-18 12:54:38 -0600

    Basic Repo setup w/ context

--- BangazonTaskTracker/DAL/TaskTrackerRepository.cs
+++ BangazonTaskTracker/DAL/TaskTrackerRepository.cs
@@ -1,1 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using BangazonTaskTracker.Models;
 
+namespace BangazonTaskTracker.DAL
+{
+    public class TaskTrackerRepository
+    {
+        public TaskTrackerContext Context { get; set; }
+        
+        public TaskTrackerRepository()
+        {
+            Context = new TaskTrackerContext();
+        }
+        
+        public TaskTrackerRepository(TaskTrackerContext _context)
+        {
+            Context = _context;
+        }
+    }
+}
